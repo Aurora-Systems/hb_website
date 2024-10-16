0e73e5aa119c34f22c37f3067a2d985bbbfc1cf9
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-10-16 16:57:34 +0200

    contact form created

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css"
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css'
 import Site from './site'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Site />
+    <ToastContainer/>
   </StrictMode>,
 )
